b84c0d4180574addac2bac7442a684a7fa97c49b
warsnapper <warsnapper@gmail.com>
2022-02-21 22:21:10 +0300

    3.2.04 is done

--- 3.2.04.py
+++ 3.2.04.py
@@ -2,14 +2,27 @@ import clr
 clr.AddReference('RevitAPI') 
 
 from Autodesk.Revit.DB import FilteredElementCollector as FEC
-from Autodesk.Revit.DB import ElementCategoryFilter, BuiltInCategory, BuiltInParameter
+from Autodesk.Revit.DB import ElementCategoryFilter, BuiltInCategory, BuiltInParameter, WallType
 
 
-filter = ElementCategoryFilter(BuiltInCategory.OST_Walls)
-elements = FEC(doc).WherePasses(filter).WhereElementIsElementType()
+# filter = ElementCategoryFilter(BuiltInCategory.OST_Walls)
+# elements = FEC(doc).WherePasses(filter).WhereElementIsElementType()
 
+# print len([element for element in elements])
+
+elements = FEC(doc).OfClass(WallType)
+
+# print len([element for element in elements])
+
+sumIds = 0
 for element in elements:
-    bprint(element.Parameter[BuiltInParameter.ALL_MODEL_TYPE_MARK].Id.IntegerValue)
-    # print type(element.Parameter[BuiltInParameter.ALL_MODEL_TYPE_MARK])
+    try:
+        # bprint(element.Parameter[BuiltInParameter.ALL_MODEL_TYPE_MARK].Id.IntegerValue)
+        sumIds += element.Parameter[BuiltInParameter.ALL_MODEL_TYPE_MARK].Id.IntegerValue
+        # print type(element.Parameter[BuiltInParameter.ALL_MODEL_TYPE_MARK])
+    except:
+        # print type(element.Category)
+        bprint(element.Category.Name)
+        bprint(element.Parameter[BuiltInParameter.ALL_MODEL_TYPE_MARK])
 
-# bprint(elements)
+print sumIds
